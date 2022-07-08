c6ed1f065b12f5e1764059fc7b023739eb84d686
adpoppo <nisiwow8@gmail.com>
2022-07-08 15:53:15 +0900

    投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,15 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new(post_params)
+  end
   
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :category_id, :link, :tag).merge(user_id: current_user.id)
+  end
+
 end
