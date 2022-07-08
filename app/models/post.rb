b10c11a993d8565c595b8fd9fb52c1ed0c9e2923
adpoppo <nisiwow8@gmail.com>
2022-07-08 16:54:23 +0900

    バリデーションの設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,11 @@ class Post < ApplicationRecord
   belongs_to :category
   has_one_attached :post_image
 
+  with_options presence: true do
+    validates :title, length: {maximum: 40}
+    validates :content, length: {maximum: 1000}
+  end
+
+  validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
+
 end
--- db/migrate/20220708004733_create_posts.rb
+++ db/migrate/20220708004733_create_posts.rb
@@ -1,9 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.references :user,    null: false, foreign_key: true
-      t.string :title,       null: false
-      t.text :content,       null: false
+      t.references :user,     null: false, foreign_key: true
+      t.string :title,        null: false
+      t.text :content,        null: false
       t.integer :category_id, null: false
       t.string :link
       t.string :tag
