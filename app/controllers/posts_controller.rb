db6a7865fc429b8f34d9499567ede74712457f8e
adpoppo <nisiwow8@gmail.com>
2022-07-11 14:13:45 +0900

    投稿更新機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,15 @@ class PostsController < ApplicationController
   def edit
     @post = Post.find(params[:id])
   end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
   
   private
 
