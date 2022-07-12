2ba4352e16ad67df40fa18e8fb3b4aaf0ac14d5a
adpoppo <nisiwow8@gmail.com>
2022-07-12 17:13:15 +0900

    dependentオプションの追加

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   belongs_to :category
-  has_many :comments
+  has_many :comments, dependent: :destroy
   has_one_attached :post_image
 
   with_options presence: true do
