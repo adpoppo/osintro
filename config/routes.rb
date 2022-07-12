0ece95591a40461ce6c2a0b67d288601b981bdae
adpoppo <nisiwow8@gmail.com>
2022-07-12 14:47:54 +0900

    ルーティングとコントローラーの設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
     registrations: "users/registrations"
   }
   root to: 'posts#index'
-  resources :posts
+  resources :posts do
+    resources :comments, only: :create
+  end
   resources :users, only: :show
 end
