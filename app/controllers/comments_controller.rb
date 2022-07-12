25dfc7fd40a6b648122fa95286fbaf3efe734292
adpoppo <nisiwow8@gmail.com>
2022-07-12 15:58:10 +0900

    コメント削除機能の実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,14 @@
 class CommentsController < ApplicationController
+  before_action :move_to_index, only: :destroy
 
   def create
     comment = Comment.create(comment_params)
+    redirect_to post_path(comment.post.id)
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id], post_id: params[:post_id])
+    comment.destroy
     redirect_to post_path(comment.post.id)
   end
 
@@ -9,6 +16,13 @@ class CommentsController < ApplicationController
 
   def comment_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
+
+  def move_to_index
+    comment = Comment.find(params[:id])
+    unless user_signed_in? && current_user.id == comment.user_id
+      redirect_to root_path
+    end
   end
 
 end
