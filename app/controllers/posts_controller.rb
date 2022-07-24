af48031d5bdbb1ece181be1b730478bca688a482
adpoppo <nisiwow8@gmail.com>
2022-07-24 16:20:35 +0900

    カテゴリー選択による絞り込み検索

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,6 +54,10 @@ class PostsController < ApplicationController
   end
   
   def seek
+    if params[:q]&.dig(:title)
+      squished_keywords = params[:q][:title].squish
+      params[:q][:title_cont_any] = squished_keywords.split(/[[:blank:]]/)
+    end
     @result = Post.ransack(params[:q]).result
   end
 
